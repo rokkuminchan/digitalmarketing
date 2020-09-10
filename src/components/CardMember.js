0be72c4ee777785df0011b9a90356083bdc5d9bf
VDHBK <huy@world-works.co.jp>
2020-09-11 08:38:17 +0900

    edit style

--- src/components/CardMember.js
+++ src/components/CardMember.js
@@ -4,7 +4,9 @@ import "./CardMember.css";
 const CardMember = ({ data }) => {
   return (
     <div className="col-3 col-s-5 cardmember">
-      <img className="cardmember__image" {...data.img} />
+      <div>
+        <img className="cardmember__image" {...data.img} />
+      </div>
       <h1 className="cardmember__title">{data.title}</h1>
       <p className="cardmember__description">{data.description}</p>
     </div>

1692a58846861347aa332e8c46f98ca7facc294c
Tran Minh Loc <weewaves@gmail.com>
2020-09-11 11:21:46 +0900

    Revert console log

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import "./App.css";
 import AboutUsPage from "./pages/AboutUsPage";
 
 function App() {
-  console.log(" ");
-
   return (
     <div className="App">
       <AboutUsPage />
--- src/components/BlockHeader.js
+++ src/components/BlockHeader.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 
-const BlockHeader = ({ heading, subHeading, content }) => {
-  return (
-    <React.Fragment>
-      <h1>{heading}</h1>
-      <p>{subHeading}</p>
-      <p>{content}</p>
-    </React.Fragment>
-  );
-};
 
+const BlockHeader = ({heading,subHeading,content}) => {
+	return (
+		<React.Fragment>
+			<h1>{heading}</h1>
+			<p>{subHeading}</p>
+			<p>{content}</p>
+		</React.Fragment>
+	);
+}
 export default BlockHeader;

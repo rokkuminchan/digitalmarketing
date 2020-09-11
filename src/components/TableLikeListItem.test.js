f23b6cc50996845403a02481909bf4877d27b9db
buixuandung275 <71059153+buixuandung275@users.noreply.github.com>
2020-09-11 13:05:57 +0900

    Update TableLikeListItem.test.js
    TableLike Unit Test

--- src/components/TableLikeListItem.test.js
+++ src/components/TableLikeListItem.test.js
@@ -5,6 +5,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { act } from "react-dom/test-utils";
 import TableLikeListItem from "./TableLikeListItem";
+import AboutUsData from "/.AboutUsData";
 
 let container;
 
@@ -21,9 +22,37 @@ afterEach(() => {
 });
 
 it("can render a view component from data", () => {
+  const TableLikeListItem = [
+    {
+      values: [
+        {
+          title: "言葉の壁",
+          description:
+            "普通の日本人と同じように、日本語の読み書きや会話ができる。",
+          css: "round-title-list-item__title--greenbg",
+        },
+        {
+          title: "文化の壁",
+          css: "round-title-list-item__title--bluebg",
+          description: "日本の文化を理解し、尊重しする。",
+        },
+        {
+          title: "業務の壁",
+          css: "round-title-list-item__title--redbg",
+          description: "日本人の働き方を学び、仕事に役立つシステムを設計する。",
+        },
+      ],
+    },
+    
+  ];
+
   act(() => {
     ReactDOM.render(<TableLikeListItem data={Data} />, container);
   });
+  const TableTitle = container.querySelector("table-like-list-item__title") ;
+  const Description = container.querySelector("table-like-list-item__description");
 
-  expect(true).toBe(true);
+
+  expect(TableTitle).toBe(TableLikeListItem.title);
+  expect(Description).toBe(TableLikeListItem.Description);
 });
